1cf21bc7c9d72fe1ece5ce626c50852a221a0120
djychen7 <kanokachannel7@gmail.com>
2025-02-20 12:56:22 +0800

    Applied black on rss related files

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,5 +4,5 @@ from blogging.views import BlogListView, BlogDetailView, LatestPostsFeed
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
-    path('feed/', LatestPostsFeed()),
+    path("feed/", LatestPostsFeed()),
 ]
